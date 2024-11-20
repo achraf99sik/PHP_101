f9d5dd900f32b9ba54e8ba135a7d5033287af6d3
achraf <achrafsikal@gmail.com>
2024-11-20 16:18:58 +0100

    var_dump() and arrays

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo 'hello php world';
+    echo '<h1>hello world</h1>';
+    var_dump((bool) "");
+    echo "<br>";
+    var_dump((bool) array(1));
+    echo "<br>";
+    var_dump((bool) []);
+    echo "<br>";
+    var_dump((bool) 0);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+        
+    ?>
+</body>
+</html>
